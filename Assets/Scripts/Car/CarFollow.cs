1660ac4594fde591f1394053e168c28e33c6c216
Hannes Rahm <me@hannesrahm.com>
2023-04-22 08:37:11 +0200

    Add Follower Cars

---
+++ Assets/Scripts/Car/CarFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CarFollow : CarBehaviour{
+	public Transform Follow;
+
+	void LateUpdate() {
+		if (Follow == null)
+			return;
+		var delta = Follow.position - transform.position;
+		delta.y = 0f;
+		delta.Normalize();
+		
+		Car.InputDirection = delta;
+		Car.InputAcceleration = 1f;
+	}
+}
